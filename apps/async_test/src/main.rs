c1822cbe5d1f9fe6fbc67b13b7451a7933cfceb4
zflcs <1491657576@qq.com>
2024-07-15 21:43:39 +0800

    1. Add support of spawn an async function. 2. Existed Bug: `async fn` resumed after completion.

--- apps/async_test/src/main.rs
+++ apps/async_test/src/main.rs
@@ -2,9 +2,16 @@
 #![cfg_attr(feature = "axstd", no_main)]
 
 #[cfg(feature = "axstd")]
-use axstd::println;
+extern crate axstd as std;
+
+use std::println;
+use std::thread;
 
 #[cfg_attr(feature = "axstd", no_mangle)]
 fn main() {
     println!("Hello, world!");
+    thread::spawn(move || async {
+        println!("Hello from a thread!");
+    });
+    loop {}
 }
